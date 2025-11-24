77c2eb6c61be6a60e39e9c001da05bece216614d
Emeraldtheporcupine <125104247+Emeraldtheporcupine@users.noreply.github.com>
2025-11-23 21:19:46 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -788,7 +788,7 @@ timer.after(4000, function () {
                     200,
                     true
                     )
-                    versionNumber = textsprite.create("v. 1.6.2")
+                    versionNumber = textsprite.create("v. 1.6.3")
                     versionNumber.setPosition(25, 114)
                     music.play(music.createSong(assets.song`TitleScreen`), music.PlaybackMode.InBackground)
                 })
